3746124605b37a6b17fa54beee5f71bc69444aab
Pedro Henrique dos Santos <phsantosti@gmail.com>
2024-07-23 14:51:07 -0300

    definir por por variavel de ambiente ou padrão

--- frontend/server.js
+++ frontend/server.js
@@ -2,8 +2,14 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, "build")));
+
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
-app.listen(process.env.PORT);
+
+app.listen(PORT, () => {
+	console.log('Listening on Port: ' + PORT);
+});
